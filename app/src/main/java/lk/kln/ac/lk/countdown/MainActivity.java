2144c675ceef71c6854694991c35ebc11761cd86
kotuvila <chayapriyadarshani1996@gmail.com>
2019-03-06 15:04:38 +0530

    text view and counting

--- app/src/main/java/lk/kln/ac/lk/countdown/MainActivity.java
+++ app/src/main/java/lk/kln/ac/lk/countdown/MainActivity.java
@@ -1,13 +1,33 @@
 package lk.kln.ac.lk.countdown;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  int counter =100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countDown();
+    }
+
+    private void countDown(){
+        final TextView textView = findViewById(R.id.textView2);
+
+        final Handler handler= new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                textView.setText(Integer.toString(counter));
+                counter--;
+                handler.postDelayed(this,1000);
+
+            }
+        });
+
     }
 }
